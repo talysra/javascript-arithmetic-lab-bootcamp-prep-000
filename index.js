1d940b4c3cc9db4386304d93522231d49f57f8c1
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-09-29 19:39:07 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function add(a, b) {
+  return a + b
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function inc(a) {
+  return a += 1;
+}
+
+function dec(a) {
+  return a -= 1
+}
+
+function makeInt(a) {
+  parseInt('a', 10)
+}
+
+function functionName() {
+  parseInt('0', 10)
+}
+
+function preserveDecimal() {
+  parseFloat('2.222')
+}
+
+function preserveDecimal() {
+  parseFloat('a')
+}
